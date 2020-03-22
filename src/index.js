154fbc94e232dc1f1e692e8ba109453e3428be2a
qlixxxy <sqlixor@gmail.com>
2020-03-22 21:10:33 +0300

    feat: complete test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+ if (array == undefined || array.length == 0) return 0
+
+
+  let arr = array.sort((a,b) => (a > b) ? 1 : 0);
+  return Math.min(...array)
+
+
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length == 0) return 0
+
+  let arr = array.sort((a,b) => b - a);
+  return arr[0]
+  
+  
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array.length == 0) return 0
+
+  return (array.reduce((result,item) => item + result, 0))/(array.length)
 }
